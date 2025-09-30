fdf473f5391a279db7153613bcbfb4d2c36a14a0
Tore Hirth <torehirth@gmail.com>
2025-09-30 18:37:31 +0200

    update sidebar functionality to initially auto hide on small screens and auto display on larger

---
+++ src/js/sidebar.mjs
@@ -0,0 +1,29 @@
+export const toggleSideBar = () => {
+  const sidebar = document.querySelector("#sidebarNav");
+
+  const setInitialState = () => {
+    if (window.innerWidth < 768) {
+      sidebar.classList.add("collapsed");
+    } else {
+      sidebar.classList.remove("collapsed");
+    }
+  };
+
+  setInitialState();
+
+  document.addEventListener("click", (e) => {
+    if (e.target.closest("#sidebar-toggle-btn")) {
+      sidebar.classList.toggle("collapsed");
+    }
+  });
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth >= 768) {
+      sidebar.classList.remove("collapsed");
+    } else {
+      sidebar.classList.add("collapsed");
+    }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", toggleSideBar);
