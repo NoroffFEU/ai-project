f0d3227fadb9f296f5d51a99b9b8c00c07db40df
Andy19955 <andy_19955@hotmail.com>
2025-10-01 19:39:07 +0200

    Add dynamic data to taskboard

---
+++ src/js/handlers/handleTaskBoardData.mjs
@@ -0,0 +1,52 @@
+import { isAuthenticated } from "/src/js/auth/isAuthenticated.mjs";
+import { createTaskBoardItem } from "/src/js/ui/createTaskBoardItem.mjs";
+import { addToLocalStorage, getFromLocalStorage } from "../localStorage.js";
+import { dropdownButtonListener } from "../listeners/dropdownButtonListener.mjs";
+
+export async function handleTaskBoardData() {
+  const taskBoardContainer = document.querySelector("#task-board-container");
+
+  if (isAuthenticated()) {
+    try {
+      const fetchTaskData = await fetch(""); // Replace with actual API endpoint
+      const taskData = await fetchTaskData.json();
+      if (taskData) {
+        taskData.tasks.forEach((task) => {
+          taskBoardContainer.appendChild(createTaskBoardItem(task));
+        });
+      }
+    } catch (error) {
+      taskBoardContainer.innerText = "No tasks available.";
+    }
+  } else {
+    try {
+      const fetchDemoData = await fetch("/src/js/demoData.json");
+      if (!fetchDemoData.ok) {
+        taskBoardContainer.innerText = "No tasks available.";
+        return;
+      }
+      const demoData = await fetchDemoData.json();
+      if (demoData.tasks.length > 0) {
+        const limitedTasks = demoData.tasks.slice(0, 4);
+
+        addToLocalStorage("tasks", JSON.stringify(limitedTasks));
+
+        const storedTasks = getFromLocalStorage("tasks");
+        const tasks = JSON.parse(storedTasks);
+        tasks.forEach((task) => {
+          taskBoardContainer.appendChild(createTaskBoardItem(task));
+        });
+
+        const completedTasks = tasks.filter((task) => task.completed);
+        document.querySelector("#completed-tasks").innerText =
+          completedTasks.length;
+        document.querySelector("#total-tasks").innerText = tasks.length;
+      } else {
+        taskBoardContainer.innerText = "No tasks available.";
+      }
+    } catch (error) {
+      taskBoardContainer.innerText = "No tasks available.";
+    }
+  }
+  dropdownButtonListener();
+}
---
+++ src/js/ui/createTaskBoardItem.mjs
@@ -0,0 +1,79 @@
+export function createTaskBoardItem(task) {
+  const liElement = document.createElement("li");
+  liElement.className =
+    "list-group-item d-flex justify-content-between align-items-center bg-black bg-opacity-10 mb-2 rounded border-0";
+
+  const taskInfoDiv = document.createElement("div");
+  taskInfoDiv.className = "d-flex align-items-center";
+
+  const checkboxContainer = document.createElement("div");
+
+  const checkboxInput = document.createElement("input");
+  checkboxInput.type = "checkbox";
+  checkboxInput.className = "form-check-input me-2";
+  checkboxInput.checked = task.completed;
+  checkboxContainer.appendChild(checkboxInput);
+
+  const taskTitle = document.createElement("span");
+  taskTitle.textContent = task.title;
+
+  taskInfoDiv.appendChild(checkboxContainer);
+  taskInfoDiv.appendChild(taskTitle);
+
+  const dropdownContainer = document.createElement("div");
+  dropdownContainer.className = "dropdown";
+
+  const dropdownButton = document.createElement("button");
+  dropdownButton.className = "btn btn-sm dropdown-button";
+  dropdownButton.type = "button";
+  dropdownButton.setAttribute("aria-expanded", "false");
+  dropdownButton.dataset.bsToggle = "dropdown";
+  dropdownButton.dataset.taskId = task.id;
+
+  const dropdownButtonIcon = document.createElement("i");
+  dropdownButtonIcon.className = "fa-solid fa-ellipsis-vertical";
+  dropdownButton.appendChild(dropdownButtonIcon);
+
+  const dropdownMenu = document.createElement("ul");
+  dropdownMenu.className = "dropdown-menu end-0";
+  dropdownMenu.dataset.taskId = task.id;
+
+  const editOption = document.createElement("li");
+  editOption.className = "dropdown-item";
+  editOption.dataset.taskId = task.id;
+  editOption.dataset.action = "edit";
+
+  const editIcon = document.createElement("i");
+  editIcon.className = "fa-solid fa-pen me-2";
+
+  const editText = document.createElement("span");
+  editText.textContent = "Edit";
+
+  editOption.appendChild(editIcon);
+  editOption.appendChild(editText);
+
+  const deleteOption = document.createElement("li");
+  deleteOption.className = "dropdown-item";
+  deleteOption.dataset.taskId = task.id;
+  deleteOption.dataset.action = "delete";
+
+  const deleteIcon = document.createElement("i");
+  deleteIcon.className = "fa-solid fa-trash me-2";
+
+  const deleteText = document.createElement("span");
+  deleteText.textContent = "Delete";
+
+  deleteOption.appendChild(deleteIcon);
+  deleteOption.appendChild(deleteText);
+
+  dropdownMenu.appendChild(editOption);
+  dropdownMenu.appendChild(deleteOption);
+
+  dropdownContainer.appendChild(dropdownButton);
+  dropdownContainer.appendChild(dropdownMenu);
+
+  liElement.appendChild(taskInfoDiv);
+  liElement.appendChild(dropdownContainer);
+
+  return liElement;
+}
