822fe3534f103e5bdd79359b4b30f8fad8818445
Nikita-stud <nikita151998de@gmail.com>
2025-10-08 13:11:57 +0200

    "Updated the braces issue, now you can use start command, fixed missing functions in index.html and found out that register user is never ran, thus turning it off"

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
 import { sidebar } from "./sidebar.js";
+import { toggleSidebar } from "./handlers/toggleSidebar.mjs";
+import { renderSidebar } from "./handlers/renderSidebar.mjs";
+import { logOut } from "./handlers/logOut.mjs";
 import { handleTaskBoardData } from "./handlers/handleTaskBoardData.mjs";
 import { loginHandler } from "./handlers/loginHandler.mjs";
-import { handleTaskBoardData } from "./handlers/handleTaskBoardData.mjs";
 
 handleTaskBoardData();
 loginHandler();
 logOut("/");
 toggleSidebar();
 renderSidebar();
-
+sidebar();
